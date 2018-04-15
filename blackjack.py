21d0c18d56a8d9bcaa0d49f39db1ba1e816d3a11
Hept4ne <Jeremyh105@gmail.com>
2018-04-15 00:41:31 -0400

    New, rewritten version of blackjack with better code.

--- blackjack.py
+++ blackjack.py
@@ -1,207 +1,201 @@
 #Blackjack.
 
-from random import randint
-from random import choice
+import sys
+from random import randint, choice
 
-gametime = True
+#Strings (too long to fit in PEP-8 guidelines otherwise):
 
-kingqueenjack = 10
+optionlist = "Hit (1), Stand (2), Double down (3), Split (4), or Surrender (5)."
 
-kqjlist = ["King!", "Queen!", "Jack!"]
+#Classes:
 
-#----------------------------------------
+class actor(object):
+    '''actor class for variables and methods shared by player and dealer'''
+    busted = False
+    gotace = False
+    turn = False
+    numofaces = 0
+    value = 0
+    ace = 11
+    kqjlist = ["King!", "Queen!", "Jack!"]
+    kingqueenjack = 10
+    def __init__(self):
+        pass
+    def gethand(self, nowworth):
+        '''Print's the person's hand. "nowworth" is a boolean for printing
+           "X's hand is now worth Y." instead of "X's hand is worth Y."'''
+        if nowworth:
+            print ("%s's hand is now worth %i."%(self.name, self.value))
+        print ("%s's hand is worth %i."%(self.name, self.value))
+    def silentcardpick(self):
+        '''Picks card with no announcement.'''
+        cpick = randint(1,14)
+        if cpick == 1:
+            print ("Ace!")
+            self.value += self.ace
+            self.gotace = True
+        elif cpick > 10:
+            print (choice(self.kqjlist))
+            self.value += self.kingqueenjack
+        else:
+            print (str(cpick))
+            self.value += cpick        
+    def cardpick(self):
+        '''Picks card with announcement.'''
+        self.silentcardpick()
+        self.gethand(False)
+    def softace(self):
+        '''Sets the ace's value to 1 and lower's the person's value by 10.'''
+        self.ace = 1
+        self.value -= 10
+        self.gethand(True)
 
-while gametime is True: #Main game loop.
-
-    playervalue = 0
-    dealervalue = 0
-    playerace = 11
-    dealerace = 11
-
-    playernumofaces = 0
-    dealernumofaces = 0
-
+class d(actor):
+    '''Dealer class.'''
+    name = "Dealer"
     holeace = False
     holekqj = False
     holecard = 0
     revealhole = True
-
-    playerinit = 0
-    playerbusted = False
-    dealerbusted = False
-    playergotace = False
-    dealergotace = False
-    playerturn = False
-    playerstand = False
-    dealerturn = False
-    roundstart = True
-    surrender = False
-    
-    if roundstart is True:
-        print ("Welcome to Blackjack.")
-        print ("-")
-        #Starts the round. Sets up the dealer's initial hand, and
-        #gives the player their initial two cards.
-        #Dealer's initial hand:
-        cardpick = randint(1,14)
-        print ("Dealer's initial hand:")
-        if cardpick == 1:
+    def __init__(self):
+        pass
+    def holecardpick(self):
+        '''Picks the dealer's hole card.'''
+        cpick = randint(1,14)
+        if cpick == 1:
+            self.holeace = True
+            self.value += self.ace
+            self.gotace = True
+        elif cpick > 10:
+            self.holekqj = True
+            self.value += self.kingqueenjack
+        else:
+            self.holecard = cpick
+            self.value += cpick
+    def revealholepick(self):
+        '''Reveals the dealer's hole card.'''
+        print ("Dealer's hole card revealed.")
+        if self.holeace:
             print ("Ace!")
-            dealervalue += dealerace
-            dealergotace = True
-        elif cardpick > 10:
-            print (choice(kqjlist))
-            dealervalue += kingqueenjack
+        elif self.holekqj:
+            print (choice(self.kqjlist))
         else:
-            print (str(cardpick))
-            dealervalue += cardpick
-        print ("Dealer's hand is worth " + str(dealervalue) + ".")
-        #Dealer's hole (hidden/face down) card:
-        cardpick = randint(1,14)
-        if cardpick == 1:
-            holeace = True
-            dealervalue += dealerace
-            dealergotace = True
-        elif cardpick > 10:
-            holekqj = True
-            dealervalue += kingqueenjack
-        else:
-            holecard += cardpick
-            dealervalue += cardpick
-        print ("-")
-        print ("Player's initial hand:")
+            print (str(self.holecard))
+
+class p(actor):
+    '''Player class.'''
+    name = "Player"
+    stand = False
+    surrender = False
+    def __init__(self):
+        pass
+
+##################
+
+playing = True
+
+while playing:
+
+    #Game variables, reset each round:
+    
+    dealer = d()
+    player = p()
+    roundstart = True
+
+    if roundstart:
+        #Starts the round. Sets up the dealer's initial hand,
+        #dealer's hole (hidden/face down) card, and
+        #gives the player their initial two cards.
+        
+        print ("Welcome to blackjack.\n---\nDealer's initial hand:")
+        dealer.cardpick()
+        dealer.holecardpick()
+        
         #Player's initial two cards:
-        while playerinit < 2:
-            cardpick = randint(1,14)
-            if cardpick == 1:
-                print ("Ace!")
-                playervalue += playerace
-                playergotace = True
-            elif cardpick > 10:
-                print (choice(kqjlist))
-                playervalue += kingqueenjack
-            else:
-                print (str(cardpick))
-                playervalue += cardpick
-            playerinit += 1
-        print ("Player's hand is worth " + str(playervalue) + ".")
-        if playervalue == 21:
-            #Checks if the player got 21 on the first two cards (blackjack/natural).
-            print ("Player got a natural 21!")
-            roundstart = False
-            playerturn = False
-            dealerturn = True
-            print ("-")
+        
+        print ("-\nPlayer's initial hand:")
+        player.silentcardpick()
+        player.cardpick()
+        if player.value == 21:
+            #Checks if player got 21 on first two cards (blackjack/natural).
+            print ("Player got a natural 21!\n-")
+            player.turn = False
+            dealer.turn = True
         else:
-            print ("-")
-            roundstart = False
-            playerturn = True
-            print ("Player's turn.")
-            #Ends the round start and begins the player's turn.
+            player.turn = True
+            print ("-\nPlayer's turn.")
+        roundstart = False
 
-    while playerturn is True: #Player's turn.
-        if (playervalue > 21 and playergotace is False) or (playervalue > 21 and playergotace is True and playerace == 1):
+    #Player's turn.
+    while player.turn:
+        if (player.value > 21 and not player.gotace)\
+           or (player.value > 21 and player.gotace and player.ace == 1):
             #Checks if the player busted. Instant lose.
-            playerturn = False
-            dealerturn = False
-            print ("-")
+            player.turn = False
+            dealer.turn = False
             break
-        if (playervalue > 21 and playergotace is True and playerace == 11):
-            #Checks if the player busted with an ace.
+        if player.value > 21 and player.gotace and player.ace == 11:
+            #Checks if the player busted with one ace.
             print ("Player had a soft bust. Ace is now worth 1.")
-            playerace = 1
-            playervalue -= 10
-            print ("Player's hand is now worth " + str(playervalue) + ".")
-        print ("Choose to Hit (1), Stand (2), Double down (3), Split (4), or Surrender (5).")
-        playerchoice = input("Choice: ")
-        while playerchoice.isdigit() is False: #Checks if the input is a digit.
+            player.softace()
+        pchoice = input("Choose to %s\n"%(optionlist) + "Choice: ")
+        while not pchoice.isdigit() or int(pchoice) not in range(1,6):
+            #If the choice is not a digit or not in the range of choices.
             print ("Invalid input.")
-            playerchoice = input("Choice: ")
-        if int(playerchoice) == 1: #If Hit is chosen, randomly selects a card.
-            cardpick = randint(1,14)
-            if cardpick == 1:
-                print ("Ace!")
-                playervalue += playerace
-                playergotace = True
-            elif cardpick > 10:
-                print (choice(kqjlist))
-                playervalue += kingqueenjack
-            else:
-                print (str(cardpick))
-                playervalue += cardpick
-            print ("Player's hand is worth " + str(playervalue) + ".")
-        elif int(playerchoice) == 2: #If Stand is chosen, end turn.
-            print ("Player stood.")
-            print ("-")
-            playerturn = False
-            dealerturn = True
-        elif int(playerchoice) == 3:
+            pchoice = input("Choice: ")
+        if int(pchoice) == 1:
+            #If Hit is chosen, randomly selects a card.
+            player.cardpick()
+        elif int(pchoice) == 2:
+            #If Stand is chosen, end turn.
+            print ("Player stood.\n-")
+            player.turn = False
+            dealer.turn = True
+        elif int(pchoice) in range(3,6):
             print ("WIP")
-        elif int(playerchoice) == 4:
-            print ("WIP")
-        elif int(playerchoice) == 5:
-            print ("WIP")
-        else: #If the inputted number is greater than 5.
-            print ("Invalid input.")
 
-    while dealerturn is True: #Dealer's turn.
-        if revealhole is True: #Reveals the hole card.
-            print ("Dealer's hole card revealed.")
-            if holeace is True:
-                print ("Ace!")
-            elif holekqj is True:
-                print (choice(kqjlist))
-            else:
-                print (str(holecard))
-            print ("Dealer's hand is worth " + str(dealervalue) + ".")
-            revealhole = False
-        while dealervalue < 17: #Dealer keeps hitting until his hand is 17 or greater.
-            print ("Dealer hits.")
-            cardpick = randint(1,14)
-            if cardpick == 1:
-                print ("Ace!")
-                dealervalue += dealerace
-                dealergotace = True
-            elif cardpick > 10:
-                print (choice(kqjlist))
-                dealervalue += kingqueenjack
-            else:
-                print (str(cardpick))
-                dealervalue += cardpick
-            print ("Dealer hand is worth " + str(dealervalue) + ".")
-        if dealervalue >= 17 and dealergotace is True and dealerace == 11:
+    #Dealer's turn.
+    while dealer.turn:
+        if dealer.revealhole:
+            #Reveals the hole card.
+            dealer.revealholepick()
+            dealer.revealhole = False
+        if dealer.value < 17:
+            #Dealer keeps hitting until their hand is 17 or greater.
+            dealer.cardpick()
+        if dealer.value >= 17 and dealer.gotace and dealer.ace == 11:
             #If the dealer's value is greater than or equal to 17,
-            #the dealer got an ace, and he hasn't had his ace revalued to one (soft hand).
-            print ("Dealer got a soft 17 (has ace card with a hand worth 17 or more).")
-            dealerace = 1
-            dealervalue -= 10
-            print ("Dealer's hand is now worth " + str(dealervalue) + ".")
-        if dealervalue == 21: #If the dealer got 21.
-            print ("Dealer got 21!")
+            #the dealer got an ace, and he hasn't had his ace
+            #revalued to one (soft hand).
+            print ("Dealer got a soft 17. Ace is now worth 1.")
+            dealer.softace()
+        if dealer.value == 21:
+            #If the dealer got 21.
+            print ("Dealer got 21!\n-")
+            dealer.turn = False
+        if dealer.value > 21:
+            #If the dealer got more than 21 (busted).
             print ("-")
-            dealerturn = False
-        if dealervalue > 21: #If the dealer got more than 21 (busted).
-            print ("-")
-            dealerturn = False
-            break
-        if (dealervalue >= 17 and dealergotace is False) or (dealervalue >= 17 and dealergotace is True and dealerace == 1):
-            #If the dealer's hand is greater than or equal to 17 without an ace,
-            #or the dealer's hand is greater than 17 with the ace already used (hard).
-            print ("Dealer stood.")
-            print ("-")
-            dealerturn = False
+            dealer.turn = False
+        if (dealer.value >= 17 and not dealer.gotace)\
+           or (dealer.value >= 17 and dealer.gotace and dealer.ace == 1):
+            #If the dealer's hand is >= 17 without an ace,
+            #or the dealer's hand  is > 17 with the ace already used (hard).
+            print ("Dealer stood.\n-")
+            dealer.turn = False
             break
 
-    if playervalue > 21:
+    #Game condition check:
+    if player.value > 21:
         print ("Player busted! You lose.")
-        playerbusted = True
-    if dealervalue > 21:
-        print ("Dealer Busted! You win!")
-        dealerbusted = True
-    if playerbusted is False and dealerbusted is False:
-        if playervalue > dealervalue:
+        player.busted = True
+    if dealer.value > 21:
+        print ("Dealer busted! You win!")
+        dealer.busted = True
+    if not (player.busted or dealer.busted):
+        if player.value > dealer.value:
             print ("Player hand is greater than dealer's. You Win!")
-        elif playervalue == dealervalue:
+        elif player.value == dealer.value:
             print ("Player's hand is the same as the dealer's. Tie.")
         else:
             print ("Player's hand is less than dealer's. You Lose.")
@@ -214,10 +208,6 @@ while gametime is True: #Main game loop.
         if newgameinput in "Yy":
             break
         elif newgameinput in "Nn":
-            gametime = False
-            break
-        else:
-            print ("Invalid input.")
-    print ("---")
-    print ("--")
-    print ("-")
+            sys.exit()
+        print ("Invalid input.")
+    print ("---\n--\n-")
